0a8dddd68fab6d3de550cc082a324792638b8282
tbranco-snaplogic <134067515+tbranco-snaplogic@users.noreply.github.com>
2025-09-24 15:41:23 +0100

    Update 1_🕵🏼‍♂️_Rapprochement de factures.py

--- pages/1_🕵🏼‍♂️_Rapprochement de factures.py
+++ pages/1_🕵🏼‍♂️_Rapprochement de factures.py
@@ -50,6 +50,11 @@ st.markdown(
     Conséquence : les clients sont **sous-facturés**.
     
     Échantillons Contrats:
+    - [Contrat OK](https://snaplogic.box.com/s/vbdo1942zjm7fsxmdx9vizoojjs3uu6q)
+    - [Contrat avec Formule de Révision de prix absente de l'ERP](https://snaplogic.box.com/s/9v4sn7hmw8mugismmftwodnnmrx2p92z)
+    - [Contrat avec Formule de Révision de prix erronée dans l'ERP](https://snaplogic.box.com/s/ao0zdxjmhfbgwwzeb0ew26acbf6z6sxn)
+
+    [Pipeline SnapLogic](https://cdn.emea.snaplogic.com/sl/designer.html#pipe_snode=65de0c5eede53772f6790592)
     """
 )
 with st.expander("Contrat conforme"):
